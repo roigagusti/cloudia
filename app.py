b24ef82715b93dd27e3f8d6bb17d480da3a02c43
Agustí Roig <gutiroma@hotmail.com>
2023-07-16 14:31:12 +0200

    next steps

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
-from classes.Classe import Airtable
+import os
+import openai
+from classes.private import api_key
+# from classes.Classe import Airtable
 
-at = Airtable()
+# at = Airtable()
 
 # FUNCTIONS
 
@@ -13,4 +16,16 @@ app = Flask(__name__)
 # Inici de la APP
 @app.route('/',methods=['GET'])
 def index(): # Home page
-    return render_template('index.html')
+
+    openai.api_key = api_key
+
+    response = openai.Completion.create(
+        model="curie-similarity-fast",
+        prompt="Create a list of 8 questions for my interview with a science fiction author:",
+        temperature=0.5,
+        max_tokens=10,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    return render_template('index.html',response=response)
